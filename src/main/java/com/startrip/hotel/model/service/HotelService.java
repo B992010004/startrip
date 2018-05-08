42f5bfe534a0f6a30852b88d6c39a100d727b3e7
林宏蒲 <yacctop@gmail.com>
2018-05-08 21:25:31 +0800

    AJAX圖片處理
    1. 動態上傳圖片
    2. 動態顯示縮圖
    
    PS:動態刪除待處理

--- src/main/java/com/startrip/hotel/model/service/HotelService.java
+++ src/main/java/com/startrip/hotel/model/service/HotelService.java
@@ -1,8 +1,11 @@
 package com.startrip.hotel.model.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class HotelService {
 
 }
