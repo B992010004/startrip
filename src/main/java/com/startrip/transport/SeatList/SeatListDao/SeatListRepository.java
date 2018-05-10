8f2819bdb38a891d5c0f45144ea28b87bc143b6c
老章 <littlereym@hotmail.com>
2018-05-10 14:58:45 +0800

    no message

--- src/main/java/com/startrip/transport/SeatList/SeatListDao/SeatListRepository.java
+++ src/main/java/com/startrip/transport/SeatList/SeatListDao/SeatListRepository.java
@@ -3,8 +3,6 @@ package com.startrip.transport.SeatList.SeatListDao;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,46 +11,57 @@ import org.springframework.stereotype.Repository;
 import com.startrip.transport.SeatList.SeatListInterface.SeatListRepositoryInterface;
 import com.startrip.transport.SeatList.SeatListModle.SeatListBean;
 
-
-
 @Repository
-public class SeatListRepository implements SeatListRepositoryInterface{
+public class SeatListRepository implements SeatListRepositoryInterface {
 
 	@Autowired
 	SessionFactory factory;
-	
-	
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
-	public List<SeatListBean> select(){
-		String hql ="FROM SeatListBean";
+	public List<SeatListBean> select() {
+		String hql = "FROM SeatListBean";
 		Session session = factory.getCurrentSession();
 		List<SeatListBean> list = new ArrayList<>();
-		list = session.createQuery(hql, SeatListBean.class).getResultList();//StationBean.class指定查詢一定是StationBean.class
+		list = session.createQuery(hql, SeatListBean.class).getResultList();// StationBean.class指定查詢一定是StationBean.class
 		return list;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public SeatListBean select(Integer id) {
-		
+
 		String hql = "FROM SeatListBean where id =:id";
 		Session session = null;
 		List<SeatListBean> list = new ArrayList<>();
 		session = factory.getCurrentSession();
-		list = session.createQuery(hql, SeatListBean.class).getResultList();//StationBean.class指定查詢一定是StationBean.class
-		
-		
-		
+		list = session.createQuery(hql, SeatListBean.class).getResultList();// StationBean.class指定查詢一定是StationBean.class
+
 		if (list.size() == 0) {
 			return null;
 		} else {
 			return list.get(0);
 		}
 	}
-	
+	@Override
+	public void update(Integer Trip, Integer SeatNumber, 
+			Integer Seat, Integer OrderNumber) {
+		String hql = "update SeatListBean set   Trip=:Trip SeatNumber=:SeatNumber Seat=:Seat OrderNumber=:OrderNumber   where id =:id";
+		Session session = factory.getCurrentSession();
+		session.createQuery(hql).setParameter("Trip", Trip).setParameter("SeatNumber", SeatNumber)
+				.setParameter("Seat", Seat).setParameter("OrderNumber", OrderNumber)
+				.executeUpdate();
+		
+	}
 	
 	@Override
-	public boolean delete (Integer id) {
+	public void insert(SeatListBean bean) {
+		Session session = factory.getCurrentSession();
+		session.save(bean);
+		
+	}
+	@Override
+	public boolean delete(Integer id) {
 		Session session = factory.getCurrentSession();
 		SeatListBean bean = select(id);
 		if (bean != null) {
@@ -62,19 +71,6 @@ public class SeatListRepository implements SeatListRepositoryInterface{
 		return false;
 	}
 
-	@Override
-	public void update(Integer Trip, Integer SeatNumber, Integer Seat, Integer OrderNumber) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void insert(SeatListBean bean) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-	
+
 }
